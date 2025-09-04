293cfc7b17c3b4b4e57b0530947cf69f3af5b5a3
azorophelie <azor.ophelie@yahoo.fr>
2025-09-04 15:26:11 +0200

    Changement de quelques petits détails

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Home from './pages/home'; 
 import About from './pages/About'; 
 import ErrorPage from './pages/errorPage';
